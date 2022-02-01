6e8f26cb386257a98846dd9ff895169ce0da7a9d
Matt Draghi <mtd5264@gmail.com>
2022-02-01 09:54:03 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { messages, contacts } from "../data";
-import Contact from "./Contact";
-import Message from "./Message";
+import ContactList from "./ContactList";
+import MessageList from "./MessageList"
+import MessageForm from "./MessageForm"
 
 console.log("Messages:", messages);
 console.log("Contacts:", contacts);
@@ -12,46 +13,9 @@ function App() {
       <ContactList contacts={contacts} />
       <main>
         <MessageList messages={messages} />
-        <form className="message-form">
-          <input placeholder="Type a message..." />
-        </form>
+        <MessageForm />
       </main>
     </div>
-  );
-}
-
-function ContactList({ contacts }) {
-  return (
-    <nav>
-      <h2>Contacts</h2>
-      <ul className="contacts">
-        {contacts.map((contact) => {
-          return <Contact name={contact.name} />;
-        })}
-      </ul>
-    </nav>
-  );
-}
-
-function MessageList({ messages }) {
-  return (
-    <>
-      <h2>Messages</h2>,
-      <section className="messages">
-        <ul>
-          {messages.map((message) => {
-            return (
-              <Message
-                key={message.id}
-                letter={message.name[0]}
-                content={message.content}
-                type={message.type}
-              />
-            );
-          })}
-        </ul>
-      </section>
-    </>
   );
 }
 
